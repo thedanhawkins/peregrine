ea7c4d456af130c5b104f950b0518e90d040e908
thedanhawkins <dan.hawkins@prophetandfalcon.co.uk>
2016-03-01 14:53:06 +0000

    Update dependencies
    Remove outdated dependencies: gulp-6-to-5 and gulp-minify-css.
    
    Update outdated dependencies: gulp-imagemin, gulp-load-plugins
    
    Add new dependency: gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('styles', function() {
     return gulp.src(['assets/stylesheets/vendor/*', 'assets/stylesheets/main.scss'])
     .pipe(plugins.sass({ style: 'expanded', includePaths: ['node_modules/foundation-sites/scss/'] }))
     .pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(plugins.minifyCss())
+    .pipe(plugins.cleanCss())
     .pipe(plugins.concat('main.css'))
     .pipe(plugins.rev())
     .pipe(gulp.dest('dist/'+projectName+'/assets/stylesheets'))
